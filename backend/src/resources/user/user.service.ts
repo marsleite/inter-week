2dc1278b1a9901799006e816f52ce8e942abc907
Marcelo <marsleite@gmail.com>
2021-12-15 11:08:16 -0300

    implementando a regra do service e buscando user no DB

--- backend/src/resources/user/user.service.ts
+++ backend/src/resources/user/user.service.ts
@@ -1,10 +1,21 @@
+import { getRepository } from 'typeorm';
+import { User } from '../../entity/User';
+
 import {UserSignIn} from './dtos/user.signin.dtos';
 import {UserSignUp} from './dtos/user.signup.dtos';
 
 export default class UserService {
 
   async signin(user: UserSignIn) {
+    const userRepository = getRepository(User);
 
+    const { email, password } = user;
+
+    const existsUser = userRepository.findOne({ where: { email, password } });
+
+    if (!existsUser) {
+      return null;
+    }
   }
 
   async signup(user: UserSignUp) {
